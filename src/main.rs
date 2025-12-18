ef56b3625a283441589397aab021f55aa802a8af
andreselcientifico <andresperezrodriguez28@gmail.com>
2025-12-17 22:11:39 -0500

    refactor: remove cancel order service from main application

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,6 @@ async fn main() -> std::io::Result<()> {
                 .service(func::handlers::update_user_profile)
                 .service(func::handlers::created_order)
                 .service(func::handlers::capture_order)
-                .service(func::handlers::cancel_order)
         )
             
     })
